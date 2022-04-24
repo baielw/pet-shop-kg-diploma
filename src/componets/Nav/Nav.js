5b8eef16a2fb136fd29c4b87d59b018da5a7dbb5
baielw <baielrozbekov@gmail.com>
2022-04-24 21:24:48 +0600

    add conponets

---
+++ src/componets/Nav/Nav.js
@@ -0,0 +1,28 @@
+import "./Nav.css";
+function Nav() {
+    return (
+
+        <nav>
+
+
+            <div class="logo">
+                <span class="pet">Pet</span>-<span class="shop">Shop</span>.kg
+                <p>easy, accessible and simple</p>
+            </div>
+
+
+            <div class="top-menu">
+                <ul>
+                    <li><a href="https://google.com">DOGS</a></li>
+                    <li><a href="https://google.com">CATS</a></li>
+                    <li><a href="https://google.com">BIRDS</a></li>
+                    <li><a href="https://google.com">Animal feed</a></li>
+                    <li><a href="https://google.com">Other</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+
+export default Nav;
