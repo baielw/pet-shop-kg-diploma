642f1c18c9225df39411aec1a65d50d56992caf0
baielw <baielrozbekov@gmail.com>
2022-05-06 12:23:48 +0600

    add commit

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -1,7 +1,9 @@
 import "./Header.css";
 function Header() {
     return (
-<header>
+<header> 
+    {/**/}
+            
 <div className="doby">
                 <h1>welcome to our online pet store</h1>
 </div>
@@ -11,7 +13,6 @@ function Header() {
                     A pet shop or pet store is a retail business which sells different kinds of animals. Pet stores also sell pet food, supplies, and accessories.
                 </p>
             </div>
-
 </header>
 
     );
---
+++ src/componets/Table/Table.js
@@ -0,0 +1,16 @@
+import "./Table.css";
+function Table() {
+    return (
+
+        <nav>
+
+<div></div>
+  <div></div>
+  <div></div>
+
+
+
+          
+        </nav>
+    );
+}
