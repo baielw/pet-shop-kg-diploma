b0975dad3655f0fb9f71a02b3889c04a01a1d869
baielw <baielrozbekov@gmail.com>
2022-05-10 22:31:09 +0600

    add img commit

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -6,15 +6,9 @@ function Header() {
 
             <div className="doby">
 
-                <div className="doby2">
-                    <h1>welcome to our online pet store</h1>
 
 
-                    <p>
-
-                        A pet shop or pet store is a retail business which sells different kinds of
-                        animals. Pet stores also sell pet food, supplies, and accessories.
-
+                  
                         <div className="table">
                             <div className="tabled">Lorem ipsum dolor</div>
                             <div className="tabled">Lorem ipsum dolor</div>
@@ -23,10 +17,8 @@ function Header() {
                             <div className="tabled">Lorem ipsum dolor</div>
                             <div className="tabled">Lorem ipsum dolor</div>
                         </div>
-                    </p>
 
 
-                </div>
 
 
             </div>
--- src/componets/Nav/Nav.js
+++ src/componets/Nav/Nav.js
@@ -3,27 +3,29 @@ function Nav() {
     return (
 
         <nav>
-
-
-
-
-
             <div className="top-menu">
                 <ul>
-                    <li> 
+                    <li>
                         <div className="logo">
-                        <span className="pet">Pet</span>-<span className="shop">Shop</span>.kg
-                        <p className="logo.p">easy, accessible and simple</p>
-                    </div>
+                            <span className="pet">Pet</span>-<span className="shop">Shop</span>.kg
+                        </div>
                     </li>
-                    <li><a href="https://google.com">HOME</a></li>
-                    <li><a href="https://google.com">animals</a></li>                    
-                    <li><a href="https://google.com">ABOUT</a></li>
-                    <li><a href="https://google.com">CONTACT</a></li>
 
+                    <div className="nav">
+
+                        <li><a href="https://google.com">HOME</a></li>
+                        <li><a href="https://google.com">animals</a></li>
+                        <li><a href="https://google.com">ABOUT</a></li>
+                        <li><a href="https://google.com">CONTACT</a></li>
+
+                    </div>
+                    <p>welcom to our sait</p>
                 </ul>
+
+
             </div>
-          
+               
+            <div className="p"> </div>
         </nav>
     );
 }
