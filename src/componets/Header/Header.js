246e3e940f544bc0c9f1ba95aeb3451387b2bfd9
baielw <baielrozbekov@gmail.com>
2022-05-13 14:32:03 +0600

    add .copy

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -5,24 +5,45 @@ function Header() {
     return (
         <header>
             {/**/}
+            <div className="conteiner">
 
+                <div className="copy">
+                    <div className="copyin "><p>
+                        Clean It Right 'Round The Pets
+                    </p></div>
 
-                <div className="table">
-                    <div className="tabled">
-                        <img src={headerImage} alt='headerImage' width={320} height={350} />
+                    <div className="copyin">
+                        <p>
+                        Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
                     </div>
-                    <div className="tabled">Lorem ipsum dolor</div>
-                    <div className="tabled">Lorem ipsum dolor</div> 
-                    <div className="tabled">Lorem ipsum dolor</div>
-                    <div className="tabled">Lorem ipsum dolor</div>
-                    <div className="tabled">Lorem ipsum dolor</div>
+                    <div className="copyin"></div>
+                    <div className="copyin">
+                        <p>Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    </div>
+                    <div className="copyin"></div>
+                    <div className="copyin">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+                   
                 </div>
 
+                {/*
+            <div className="table">
+                <div className="tabled">
+                    <img src={headerImage} alt='headerImage' width={320} height={400} />
+                </div>
+                <div className="tabled">Lorem ipsum dolor</div>
+                <div className="tabled">Lorem ipsum dolor</div>
+                <div className="tabled">Lorem ipsum dolor</div>
+                <div className="tabled">Lorem ipsum dolor</div>
+                <div className="tabled">Lorem ipsum dolor</div>
+            </div>
+
+*/}
 
 
 
 
-
+            </div>
         </header>
 
     );
